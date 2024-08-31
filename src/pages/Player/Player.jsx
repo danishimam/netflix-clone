566d07978b249bc69850230b6fa2075735f5ce2c
danishimam <danishimam43@gmail.com>
2024-08-31 16:19:35 +0530

    netflix clone

---
+++ src/pages/Player/Player.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react'
+import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useNavigate, useParams } from 'react-router-dom'
+
+const Player = () => {
+
+  const {id} = useParams();
+  const navigate = useNavigate();
+
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    typeof: ""
+  })
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZGQzMjNjYmNiNmYyMjNhOWE2OWY2ZDI4N2NiYjRlZiIsIm5iZiI6MTcyNDg0MTg0MS4xMzA3NDksInN1YiI6IjY2Y2VmY2NhNDJmOGJkNTRjMGJiMjA3ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.m32VJgUHTFBcixBWAEWuC7DN_Hd0Is9IkLQ_66khBEI'
+    }
+  };
+  
+  useEffect(()=>{
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results[0]))
+    .catch(err => console.error(err));
+  },[])
+
+
+  return (
+    <div className='player'>
+      <img src={back_arrow_icon} alt="" onClick={()=>{navigate(-2)}}/>
+      <iframe width='90%' height='90%' 
+      src={`https://www.youtube.com/embed/${apiData.key}`}
+      title='trailer' frameBorder='0' allowFullScreen></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0,10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Player
